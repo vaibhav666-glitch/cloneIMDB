f3f9c6a61a3cda7d231aa6b460442147f98f2752
vaibhav <vaibhavning@gmail.com>
2024-03-03 21:38:11 +0530

    Add movie page

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const movieSearchBox=document.getElementById('movie-search-box')
 const searchList=document.getElementById('search-list')
 const resultGrid=document.getElementById('result-grid')
@@ -28,11 +29,13 @@ function displayMovieList(movies)
     searchList.innerHTML=" "
     for(let i=0;i<movies.length;i++)
     {
+        // let anchorTag=document.createElement('a');
+        // anchorTag.href="MoviePage.html";
         let movieListItem=document.createElement('div');
         movieListItem.classList.add('search-list-item');  
-        movieListItem.addEventListener('click',()=>{
-            loadMoviesDetails(movies[i].imdbID);
-        })
+        movieListItem.addEventListener('click', () => {
+            window.location.href = `MoviePage.html?movieID=${movies[i].imdbID}`;
+        });
         if(movies[i].Poster !== "N/A")
           var moviePoster =movies[i].Poster;
        movieListItem.innerHTML=`
@@ -44,39 +47,43 @@ function displayMovieList(movies)
        <p>${movies[i].Year}</p>
      </div>
        `
-       searchList.appendChild(movieListItem)
+       //anchorTag.appendChild(movieListItem);
+       searchList.appendChild(movieListItem);
     }
 }
 
-async function loadMoviesDetails(movie)
-{
-   searchList.classList.add('hide-search-list');
-   movieSearchBox.value='';
-   const result= await fetch(`https://www.omdbapi.com/?i=${movie}&apikey=9d5b69f2`)
-    const movieDetails=await result.json();
-    console.log(movieDetails)
-    resultGrid.innerHTML=`  <div class="movie-poster">
-    <img src="${movieDetails.Poster}">
-  </div>
-  <div class="movie-info">
-    <h3 class="movie-title">
-      ${movieDetails.Title}
-    </h3>
-    <ul class="movie-misc-info">
-      <li class="year"><b>Year: </b>${movieDetails.Year}</li>
-      <li class="rated"><b>Rated:</b>${movieDetails.Rated}</li>
-      <li class="released"><b>Release: </b>${movieDetails.Released}</li>
-    </ul>
-    <p class="genre"><b>Genre:</b>${movieDetails.Genre}</p>
-    <p class="writer"><b>Writer:</b> ${movieDetails.Writer}</p>
-    <p class="actors"><b>Actors:</b> ${movieDetails.Actors}</p>
-    <p class="plot"><b>Plot:</b>${movieDetails.Plot}</p>
-    <p class="language"><b>Language:</b>${movieDetails.Language}</p>
-    <p class="awards"><b>Awards:</b>${movieDetails.Awards}</p>
-  </div>`
+async function loadMoviesDetails(movieID) {
+    const result = await fetch(`https://www.omdbapi.com/?i=${movieID}&apikey=9d5b69f2`);
+    const movieDetails = await result.json();
+    return movieDetails;
 }
 
-window.addEventListener('click', (event)=>{
-    if(event.target.className!=="form-control")
-        searchList.classList.add('hide-search-list');
-})
+const urlParams = new URLSearchParams(window.location.search);
+const movieID = urlParams.get('movieID');
+
+// Call loadMoviesDetails with the movieID to fetch movie details
+loadMoviesDetails(movieID)
+    .then(movieDetails => {
+        // Populate the result container on the result page with the movie details
+        document.getElementById('result-grid').innerHTML = `
+            <div class="movie-poster">
+                <img src="${movieDetails.Poster}">
+            </div>
+            <div class="movie-info">
+                <h3 class="movie-title">${movieDetails.Title}</h3>
+                <ul class="movie-misc-info">
+                    <li class="year"><b>Year: </b>${movieDetails.Year}</li>
+                    <li class="rated"><b>Rated: </b>${movieDetails.Rated}</li>
+                    <li class="released"><b>Release: </b>${movieDetails.Released}</li>
+                </ul>
+                <p class="genre"><b>Genre: </b>${movieDetails.Genre}</p>
+                <p class="writer"><b>Writer: </b>${movieDetails.Writer}</p>
+                <p class="actors"><b>Actors: </b>${movieDetails.Actors}</p>
+                <p class="plot"><b>Plot: </b>${movieDetails.Plot}</p>
+                <p class="language"><b>Language: </b>${movieDetails.Language}</p>
+                <p class="awards"><b>Awards: </b>${movieDetails.Awards}</p>
+            </div>`;
+    })
+    .catch(error => {
+        console.error(error);
+    });
